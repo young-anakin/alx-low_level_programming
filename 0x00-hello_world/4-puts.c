9fd91d2ac5962ddc19c654315c5404b585e77afd
young-anakin <antzemz@gmail.com>
2022-03-10 14:03:00 -0800

    messages

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- *  main - main block*
- *
- *  Return: Always 0 (Success) 
+ *  main - main block
+ *  Return: Always 0 
  */
 int main(void)
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-         	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
