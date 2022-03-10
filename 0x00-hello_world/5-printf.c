8ecf138071c943abedb761d51d3f44c3b5790932
young-anakin <antzemz@gmail.com>
2022-03-10 12:25:20 -0800

    hopeful

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-
-
 /**
  *
  *  * main - Entry point
@@ -11,9 +8,7 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-
 int main(void)
-
 {
 
 		printf("with proper grammar, but the outcome is a piece of art,\n");
