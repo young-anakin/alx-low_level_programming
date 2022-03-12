f34cdf41b035374481ef888e3c361ff17f92774c
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-12 03:28:50 -0800

    another commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+	while(x <= 'z')
+	{
+	putchar(x);
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
