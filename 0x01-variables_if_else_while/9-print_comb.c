03f0d0d78310b33d29ac975914a9a60b43d459c7
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-12 06:33:00 -0800

    final

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,2 +1,20 @@
+#include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+      int a = '0';
+      int b = '8';
+      while (a <= b )
+      {
+      putchar(a);
+      putchar(',');
+      putchar(' ');
+      a++;
+      }
+putchar('9');
+return (0);
+}
