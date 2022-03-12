75eb20a1ee74bb80f3817f7d57341a5d21099a54
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-12 06:56:18 -0800

    accetp

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	char x ='a';
+	while (x <= 'z')
+	{
+	while (x != 'q' && x!= 'e')
+	{
+	putchar(x);
+	goto s;
+	}
+s :
+	x++;
+	
+	}
+return (0);
+}
+	
