f603f04d7761fd728265339df0d58e042eb9c0a4
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-12 05:01:52 -0800

    another one

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	char x = 'z';
+	while (x >= 'a')
+	{
+	putchar(x);
+	x--;
+	}
+	putchar('\n');
+return (0);
+}
