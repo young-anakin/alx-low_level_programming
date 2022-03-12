14dacdc60944068fd6e71e11b72413bcf3fa7731
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-12 05:14:48 -0800

    trial

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	int x = '0';
+	char a = 'a';
+	
+	while( x <= '9')
+	{
+	putchar(x);
+	x++;
+	}
+	while( a <= 'f')
+	{
+	putchar(a);
+	a++;
+	}
+return (0);
+}
