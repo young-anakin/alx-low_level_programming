dc189b24db92a3330b79a00d46adfa624ead8627
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 04:14:27 -0700

    cm

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	char x[8] = "_putchar";
+	while(x<9)
+	{
+		put
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This is for project 0x02 of alx project
