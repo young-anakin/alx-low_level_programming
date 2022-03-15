5e51753a997e9d671800d3871f863b7f0b60f95e
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 11:50:36 -0700

    yerr

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _abs - Return absolute value of a number
- * @n - int type number
- * Return: absolute value of the number
+ * _abs - function that prints the absolute value of a number
+ * @n: takes in an integer
+ * Return: 1 if > 0, 0 if == 0, positive if negative
  */
 int _abs(int n)
 {
