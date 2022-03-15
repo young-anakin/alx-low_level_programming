442bf8de8fbff215b2c4175f89292fc5d09608a9
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 08:09:19 -0700

    msg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return:0
+ * 
+ * print_alphabet - print alphabets 10 times
  */
 void print_alphabet_x10(void)
 {
 	int w = 0;
-	while (w < 9)
+
+	while (w <= 9)
 	{
 	char x = 'a';
+
 	while (x <= 'z')
 	{
 		_putchar(x);
