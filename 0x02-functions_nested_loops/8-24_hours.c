940a12da9bcaa65c9e5f37cb639e3c6d0a452f2c
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 11:46:22 -0700

    final

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,22 @@
-#include "main."
-/** 
- * hello
- * *
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day 00:00 to 23:59
+ * @void - no argument
  */
 void jack_bauer(void)
 {
-	int x = 0;
-	while ( x< 24)
+	int i, j;
+
+	for (i = 0; i < 24; i++)
 	{
-		_putchar(
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
