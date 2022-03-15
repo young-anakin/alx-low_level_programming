880816be495d954a45d2ad3ba15156b8d9623c24
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 08:30:59 -0700

    yes

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - Entry point 
+*
+* Return : Always 0 (success)
+*/
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
