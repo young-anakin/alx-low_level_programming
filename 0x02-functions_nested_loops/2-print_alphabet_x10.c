3f518ce77d8669ad12657fea3ad62273ce899079
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 06:21:20 -0700

    aha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,4 +3,18 @@
  * main - Entry point
  * Return:0
  */
-
+void print_alphabet_x10(void)
+{
+	char x = 'a';
+	int w = 0;
+	while (w < 9)
+	{
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+	w++;
+	}
+	_putchar('\n');
+}
