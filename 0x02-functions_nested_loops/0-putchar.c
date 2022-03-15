f70aba222595ef7b685ee01404992fe5f8cb46bf
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 04:32:45 -0700

    another one

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Return:0
  */
 int main(void)
 {
-	char x[8] = "_putchar";
-	for(int i=0 ; i< 8; i++)
-	{
-		_putchar(x[i]);
-	}
+	printf("_putchar");
 	return (0);
 }
 
