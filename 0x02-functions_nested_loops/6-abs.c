ed52951d99fad09110d573945c2381f2d1f48022
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 09:43:50 -0700

    yes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Return absolute value of a number
+ * @n: char type letter
+ * Return: absolute value of the number
+ */
+int _abs(int x)
+{
+	if (x >=0)
+	{
+		return (x);
+	}
+	else {
+		return (-x);
+	}
+}
