e01171129e3f9c1c4d95f5423160780737b083f7
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 08:42:27 -0700

    yes

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Return 1,0 or -1 depending on the sign
+ * @n: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
+ */
+int print_sign(int n)
+{
+	if(n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if(n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,5 @@
 int _putchar (char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int print_sign(int n);
+int _islower(int c);
