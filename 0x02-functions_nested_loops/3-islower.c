91e402c23836ebe11d75be0761079e1c2f79502d
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 08:49:43 -0700

    spell

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* main - Entry point 
-*
-* Return : Always 0 (success)
-*/
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
+ */
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
