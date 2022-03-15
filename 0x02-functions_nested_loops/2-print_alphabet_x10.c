dd33737270506e0d68ef67529671e7b3be95f1b6
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-15 08:01:10 -0700

    msg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,16 @@
  */
 void print_alphabet_x10(void)
 {
-	char x = 'a';
 	int w = 0;
 	while (w < 9)
 	{
+	char x = 'a';
 	while (x <= 'z')
 	{
 		_putchar(x);
 		x++;
 	}
 	w++;
-	}
 	_putchar('\n');
+	}
 }
