7c3a7e424a9d277fcb3110ed1939f6d85e273bfb
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-21 08:32:40 -0700

    second attempt

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,13 +5,12 @@
  */
 int _strlen(char *s)
 {
-	int count =0;
-	for(int i=0;i<100;i++)
+	int count = 0;
+	int i = 0;
+	
+	while (s[i] != '\0')
 	{
-		if(s[i] == '\0')
-		{
-			break;
-		}
 		count ++;
 	}
+	return count;
 }
