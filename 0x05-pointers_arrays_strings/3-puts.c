4a8a707eef96b1de9cbb6e09aa655a1a4afda944
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-21 10:12:52 -0700

    fourth

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * _puts -  prints a string, followed by a new line
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string to be input
  **/
 void _puts(char *str)
