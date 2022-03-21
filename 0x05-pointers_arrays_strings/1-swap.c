74e37427e99c246ead4567238a1be8f697b0b3fc
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-21 07:51:58 -0700

    1-swamp.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b =c;
+}
