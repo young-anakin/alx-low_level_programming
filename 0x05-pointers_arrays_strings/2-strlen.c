a45f3ebc33994c8e528b4668d16fdb489db24136
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-21 08:39:02 -0700

    third attempt

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,15 @@
 /**
  * _strlen - returns the length of a string
  * @s: string that is input
+ * @count: returns the size of the string
  */
 int _strlen(char *s)
 {
 	int count = 0;
-	int i = 0;
-	
-	while (s[i] != '\0')
+
+	for(int i=0;s[i]!='\0';i++)
 	{
-		count ++;
+	count++;
 	}
 	return count;
 }
