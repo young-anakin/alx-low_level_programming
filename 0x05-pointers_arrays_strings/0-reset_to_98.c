b3785c81ee34d1741e3023301d4152b9a11fe795
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2022-03-21 07:03:24 -0700

    second attempt

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
-/* void_reset_to_90 - updates value of n to 98
+/* reset_to_98 - updates value of n to 98
  * the pointer n - supposed to reset value
- */
+ **/
 void reset_to_98(int *n)
 {
 	*n = 98;
